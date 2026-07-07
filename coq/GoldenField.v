6d65143857e29acdc178468c3e2a74303bb25da8
Brian Thorne <bthornemail@gmail.com>
2026-07-07 19:59:45 +0000

    fxed

--- coq/GoldenField.v
+++ coq/GoldenField.v
@@ -8,6 +8,7 @@
 From Coq Require Import QArith.QArith.
 From Coq Require Import QArith.Qreduction.
 From Coq Require Import QArith.Qring.
+From Coq Require Import ZArith.ZArith.
 From Coq Require Import micromega.Lia.
 From Coq Require Import Reals.Reals.
 Open Scope Q_scope.
@@ -16,6 +17,9 @@ Record Golden : Type := mkGolden {
   gr : Q;
   gs : Q
 }.
+
+Definition geq (x y : Golden) : Prop :=
+  gr x == gr y /\ gs x == gs y.
 
 Definition gr0 : Golden := mkGolden 0 0.
 Definition gr1 : Golden := mkGolden 1 0.
@@ -53,20 +57,48 @@ Fixpoint g_pow (x : Golden) (n : nat) : Golden :=
 Lemma Qnum_Qden_inj n1 d1 n2 d2 : n1 = n2 -> d1 = d2 -> Qmake n1 d1 = Qmake n2 d2.
 Proof. intros; subst; reflexivity. Qed.
 
+Lemma Z_five_mul_num (z : Z) :
+  match z with
+  | Z0 => Z0
+  | Z.pos y => Z.pos (y + y~0~0)
+  | Z.neg y => Z.neg (y + y~0~0)
+  end = (5 * z)%Z.
+Proof.
+  destruct z; simpl; ring.
+Qed.
+
 Ltac close_pos :=
-  repeat rewrite ?Pos.mul_assoc;
-  try (rewrite Pos.mul_comm; repeat rewrite ?Pos.mul_assoc);
-  try apply Pos.mul_1_l; try apply Pos.mul_1_r;
-  try reflexivity.
+  apply Pos2Z.inj;
+  repeat rewrite ?Pos2Z.inj_mul;
+  ring.
+
+Ltac normalize_z_num :=
+  repeat match goal with
+  | |- context [
+      match ?z with
+      | Z0 => Z0
+      | Z.pos y => Z.pos (y + y~0~0)
+      | Z.neg y => Z.neg (y + y~0~0)
+      end
+    ] => rewrite (Z_five_mul_num z)
+  end;
+  repeat rewrite ?Pos2Z.inj_mul;
+  repeat rewrite ?Pos2Z.inj_add;
+  repeat rewrite ?Pos2Z.inj_xO;
+  repeat rewrite ?Pos2Z.inj_xI.
+
+Ltac close_qmake :=
+  apply Qnum_Qden_inj; [normalize_z_num; ring | close_pos].
 
 Ltac golden_eq :=
+  intros;
   repeat match goal with
   | x : Golden |- _ => destruct x
   | q : Q |- _ => destruct q
   end;
   unfold g_add, g_neg, g_mul, g_sub, g_conj, gr0, gr1, phi, phi_inv, half, neg_half, Qplus, Qmult;
   simpl;
-  f_equal; f_equal; [ring | close_pos | ring | close_pos].
+  f_equal; close_qmake.
 
 Theorem g_add_assoc : forall x y z : Golden,
   g_add (g_add x y) z = g_add x (g_add y z).
@@ -91,20 +123,27 @@ Proof.
 Qed.
 
 Theorem g_mul_assoc : forall x y z : Golden,
-  g_mul (g_mul x y) z = g_mul x (g_mul y z).
-Proof. golden_eq. Qed.
+  geq (g_mul (g_mul x y) z) (g_mul x (g_mul y z)).
+Proof.
+  intros x y z.
+  destruct x, y, z; unfold geq, g_mul; simpl; split; ring.
+Qed.
 
 Theorem g_mul_comm : forall x y : Golden,
   g_mul x y = g_mul y x.
 Proof. golden_eq. Qed.
 
 Theorem g_mul_1_l : forall x : Golden,
-  g_mul gr1 x = x.
-Proof. golden_eq. Qed.
+  geq (g_mul gr1 x) x.
+Proof.
+  intro x; destruct x; unfold geq, g_mul, gr1; simpl; split; ring.
+Qed.
 
 Theorem g_mul_1_r : forall x : Golden,
-  g_mul x gr1 = x.
-Proof. golden_eq. Qed.
+  geq (g_mul x gr1) x.
+Proof.
+  intro x; destruct x; unfold geq, g_mul, gr1; simpl; split; ring.
+Qed.
 
 Theorem g_mul_0_l : forall x : Golden,
   gr (g_mul gr0 x) == 0 /\ gs (g_mul gr0 x) == 0.
@@ -119,24 +158,36 @@ Proof.
 Qed.
 
 Theorem g_distrib_l : forall x y z : Golden,
-  g_mul x (g_add y z) = g_add (g_mul x y) (g_mul x z).
-Proof. golden_eq. Qed.
+  geq (g_mul x (g_add y z)) (g_add (g_mul x y) (g_mul x z)).
+Proof.
+  intros x y z.
+  destruct x, y, z; unfold geq, g_mul, g_add; simpl; split; ring.
+Qed.
 
 Theorem g_distrib_r : forall x y z : Golden,
-  g_mul (g_add x y) z = g_add (g_mul x z) (g_mul y z).
-Proof. golden_eq. Qed.
+  geq (g_mul (g_add x y) z) (g_add (g_mul x z) (g_mul y z)).
+Proof.
+  intros x y z.
+  destruct x, y, z; unfold geq, g_mul, g_add; simpl; split; ring.
+Qed.
 
 Theorem g_conj_involutive : forall x : Golden,
-  g_conj (g_conj x) = x.
-Proof. golden_eq. Qed.
+  geq (g_conj (g_conj x)) x.
+Proof.
+  intro x; destruct x; unfold geq, g_conj; simpl; split; ring.
+Qed.
 
 Theorem g_conj_add : forall x y : Golden,
-  g_conj (g_add x y) = g_add (g_conj x) (g_conj y).
-Proof. golden_eq. Qed.
+  geq (g_conj (g_add x y)) (g_add (g_conj x) (g_conj y)).
+Proof.
+  intros x y; destruct x, y; unfold geq, g_conj, g_add; simpl; split; ring.
+Qed.
 
 Theorem g_conj_mul : forall x y : Golden,
-  g_conj (g_mul x y) = g_mul (g_conj x) (g_conj y).
-Proof. golden_eq. Qed.
+  geq (g_conj (g_mul x y)) (g_mul (g_conj x) (g_conj y)).
+Proof.
+  intros x y; destruct x, y; unfold geq, g_conj, g_mul; simpl; split; ring.
+Qed.
 
 Theorem g_norm_sq_mul : forall x y : Golden,
   g_norm_sq (g_mul x y) == g_norm_sq x * g_norm_sq y.
